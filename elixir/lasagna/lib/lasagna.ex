9a32ecd65e9865b3681e78afddd4b45d8e1fcdfa
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-01 00:10:12 -0500

    elixir/lasagna solution

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,27 @@
 defmodule Lasagna do
   # Please define the 'expected_minutes_in_oven/0' function
+  def expected_minutes_in_oven do
+    40
+  end
 
   # Please define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(timeElapsed) do
+    expected_minutes_in_oven() - timeElapsed
+  end
 
   # Please define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(nLayers) do
+    nLayers * 2
+  end
 
   # Please define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(nLayers, timeElapsed) do
+    timeElapsed + preparation_time_in_minutes(nLayers)
+  end
 
   # Please define the 'alarm/0' function
+  def alarm do
+    "Ding!"
+  end
+
 end
