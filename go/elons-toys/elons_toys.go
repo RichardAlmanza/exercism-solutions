522f87e9db935a8d882c942568c8e9b135c3059b
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-01 18:09:22 -0500

    go/elons-toys solution

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,9 +1,29 @@
 package elon
 
-// TODO: define the 'Drive()' method
+import "fmt"
 
-// TODO: define the 'DisplayDistance() string' method
+// Drive method drives the car if the car's battery is greater
+// than its batteryDrain. no return
+func (car *Car) Drive() {
+	if car.battery > car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+}
 
-// TODO: define the 'DisplayBattery() string' method
+// DisplayDistance method returns the car's driven distance as a string
+func (car Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+// DisplayBattery method returns the current car's battery percentage as a string
+func (car Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%",car.battery)
+}
+
+// CanFinish method
+func (car Car) CanFinish(trackDistance int) bool {
+	var maxDistance int =  car.speed * car.battery / car.batteryDrain
+
+	return maxDistance >= trackDistance
+}
