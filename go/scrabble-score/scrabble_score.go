3662acdab18e0b9a2e11d11bb37b26159b283eeb
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-17 17:23:08 -0500

    go/scrabble-score solution

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,48 @@
 package scrabble
 
+import "strings"
+
+type scoreMap map[rune]uint8
+
+var (
+	singleton scoreMap
+)
+
+func newScoreMap() scoreMap {
+	if singleton == nil {
+		singleton = make(scoreMap, 28)
+
+		singleton.addLetters("AEIOULNRST", 1)
+		singleton.addLetters("DG", 2)
+		singleton.addLetters("BCMP", 3)
+		singleton.addLetters("FHVWY", 4)
+		singleton.addLetters("K", 5)
+		singleton.addLetters("JX", 8)
+		singleton.addLetters("QZ", 10)
+	}
+
+	return singleton
+}
+
+func (sm scoreMap) addLetters(letters string, score uint8) {
+	for _, r := range letters {
+		sm[r] = score
+	}
+}
+
+func (sm scoreMap) getScore(word string) int  {
+	var finalScore int
+
+	word = strings.ToUpper(word)
+
+	for _, r := range word {
+		finalScore += int(sm[r])
+	}
+
+	return finalScore
+}
+
+// Score calculates the score for the scrabble word
 func Score(word string) int {
-	panic("Please implement the Score function")
+	return newScoreMap().getScore(word)
 }
