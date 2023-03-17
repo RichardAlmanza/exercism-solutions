71a5f79b6f62f9ec2c63ddb9814ff4e954ced582
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-17 18:17:34 -0500

    go/isogram solution

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,26 @@
 package isogram
 
+import "strings"
+
+// IsIsogram return if the word is a isogram
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	word = strings.ToLower(word)
+	letters :=  make(map[rune]struct{})
+
+	for _, char := range word {
+		_, exists := letters[char]
+
+		if exists {
+			return false
+		}
+
+		switch char {
+		case ' ', '-':
+			continue
+		}
+
+		letters[char] = struct{}{}
+	}
+
+	return true
 }
