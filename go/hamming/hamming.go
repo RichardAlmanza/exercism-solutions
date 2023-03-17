ae96052a09752fbe6c31e149eb36d75c56180262
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-17 14:00:20 -0500

    go/hamming solution

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,25 @@
+// hamming calculates the hamming distances
 package hamming
 
+import "errors"
+
+// Distance returns the hamming distance between two DNA strings
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	var err error
+	var distance int
+	var lenDifference int = len(a) - len(b)
+
+	if lenDifference != 0 {
+		err = errors.New("these DNA sequences don't length match")
+
+		return distance, err
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
