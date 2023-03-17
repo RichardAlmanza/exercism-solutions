e5cdb196e45552a768a6d51f31ed25c950879207
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-17 14:37:32 -0500

    go/raindrops solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,29 @@
 package raindrops
 
+import (
+	"strconv"
+	"strings"
+)
+
+// Convert returns the corresponding sound for number of raindrops
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	// Yes, I'm going to kill a fly with a snipe
+	var result string
+	var stringBuilder strings.Builder
+	factors := [3]int{3, 5, 7}
+	sounds := [3]string{"Pling", "Plang", "Plong"}
+
+	for i, factor := range factors {
+		if number % factor == 0 {
+			stringBuilder.WriteString(sounds[i])
+		}
+	}
+
+	result = stringBuilder.String()
+
+	if result == "" {
+		result = strconv.Itoa(number)
+	}
+
+	return result
 }
