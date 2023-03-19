4dea2e7c5235e7468a4cc927a57186378988b0b1
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-18 22:36:43 -0500

    go/leap solution

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,15 +1,21 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// leap determines if a given year is leap
 package leap
 
-// IsLeapYear should have a comment documenting it.
+// IsLeapYear returns if the year is leap
 func IsLeapYear(year int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	panic("Please implement the IsLeapYear function")
+	var isLeap bool
+
+	// By400 || ( !By100 && By4 )
+	switch {
+	case year % 400 == 0:
+		isLeap = true
+
+	case year % 100 == 0:
+		isLeap = false
+
+	case year % 4 == 0:
+		isLeap = true
+	}
+
+	return isLeap
 }
