4142491c62768d1885ea43a6abc3505741ad641b
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-18 21:00:29 -0500

    go/collatz-conjecture solution

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,26 @@
 package collatzconjecture
 
+import "errors"
+
+// CollatzConjecture return the
+// number of steps to reach 1 given
+// n as starting point
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	var steps int
+
+	if n <= 0 {
+		return 0, errors.New("the given number needs to be more than 0")
+	}
+
+	for n > 1 {
+		steps ++
+
+		if n % 2 == 0 {
+			n = n / 2
+		} else {
+			n = 3 * n + 1
+		}
+	}
+
+	return steps, nil
 }
