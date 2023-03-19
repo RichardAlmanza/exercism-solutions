58ad915fa10d19a3a62ad3e81c6d180e9427f87f
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-18 20:07:56 -0500

    go/difference-of-squares solution

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,60 @@
 package diffsquares
 
+type memoizationMap map[int]int
+type sumMemoization struct{
+	squareOfSum memoizationMap
+	sumOfSquares memoizationMap
+}
+
+var (
+	singleton *sumMemoization
+)
+
+func NewSumMemoization() *sumMemoization {
+	if singleton == nil {
+		singleton = new(sumMemoization)
+		singleton.squareOfSum = make(memoizationMap)
+		singleton.sumOfSquares = make(memoizationMap)
+	}
+
+	return singleton
+}
+
+func (mm memoizationMap) get(n int, formula func(int)int) int{
+	value, exists := mm[n]
+
+	if !exists {
+		value = formula(n)
+
+		mm[n] = value
+	}
+
+	return value
+}
+
+func (sm *sumMemoization) GetSquareOfSum(n int) int {
+	return sm.squareOfSum.get(n, func(i int) int {
+		value := i * (i + 1) / 2
+		value = value * value
+
+		return value
+	})
+}
+
+func (sm *sumMemoization) GetSumOfSquares(n int) int {
+	return sm.sumOfSquares.get(n, func(i int) int {
+		return i * (i + 1) * (2 * i + 1) / 6
+	})
+}
+
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	return NewSumMemoization().GetSquareOfSum(n)
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	return NewSumMemoization().GetSumOfSquares(n)
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
