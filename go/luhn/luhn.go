c86f9b3892b200e70440c80520cbcee465395e09
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-19 00:44:59 -0500

    go/luhn solution

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,44 @@
 package luhn
 
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+// Valid return if the given id is valid using the luhn algorithm
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	var digit int
+	var checkSum int
+	var lenId int
+	var isIdEven bool
+
+	id = strings.ReplaceAll(id, " ", "")
+	lenId = utf8.RuneCountInString(id)
+
+	if lenId < 2 {
+		return false
+	}
+
+	isIdEven = lenId % 2 == 0
+
+	for i, r := range id {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+
+		digit = int(r - '0')
+
+		if (i % 2 == 0) == isIdEven {
+			checkSum += ( 2 * digit) % 10
+
+			if digit > 4 {
+				checkSum++
+			}
+		}else{
+			checkSum += digit
+		}
+	}
+
+	return checkSum % 10 == 0
 }
