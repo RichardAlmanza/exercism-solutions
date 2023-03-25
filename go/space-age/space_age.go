cccf0167a9c3dd88ed873d8983c8a2a8508e8a49
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-24 19:41:54 -0500

    go/space-age solution

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -2,6 +2,44 @@ package space
 
 type Planet string
 
+const earthYearInSeconds float64 = 31557600
+
+func (p Planet) getFactor() float64 {
+	// factor is based on a year in Earth
+	var factor float64
+
+	switch p {
+	case "Mercury":
+		factor = 0.2408467
+	case "Venus":
+		factor = 0.61519726
+	case "Earth":
+		factor = 1.0
+	case "Mars":
+		factor = 1.8808158
+	case "Jupiter":
+		factor = 11.862615
+	case "Saturn":
+		factor = 29.447498
+	case "Uranus":
+		factor = 84.016846
+	case "Neptune":
+		factor = 164.79132
+	}
+
+	return factor
+}
+
+func (p Planet) getYears(seconds float64) float64 {
+	if p.getFactor() == 0.0 {
+		return -1
+	}
+
+	return seconds / ( earthYearInSeconds * p.getFactor() )
+}
+
+// Age returns how many years are on the planet, the years are
+// calculated based on the seconds' input.
 func Age(seconds float64, planet Planet) float64 {
-	panic("Please implement the Age function")
+	return planet.getYears(seconds)
 }
