e69e5f7945be0c311e3500e3cd870732418863cf
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-26 14:11:14 -0500

    go/rna-transcription solution

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,5 +1,22 @@
 package strand
 
+import "strings"
+
+type translation map[rune]rune
+
+var dnaToRna = translation{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	var newRna strings.Builder
+
+	for _, nucleotide := range dna {
+		newRna.WriteRune(dnaToRna[nucleotide])
+	}
+
+	return newRna.String()
 }
