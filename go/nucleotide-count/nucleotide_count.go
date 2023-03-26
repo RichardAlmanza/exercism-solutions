aef3212ebfe14e5b01df86adc5c447365b1308f2
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-26 13:10:50 -0500

    go/nucleotide-count solution

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -1,11 +1,21 @@
 package dna
 
-// Histogram is a mapping from nucleotide to its count in given DNA.
-// Choose a suitable data type.
-type Histogram
+import "errors"
 
-// DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]uint16
+
+func newHistogram() Histogram {
+	return Histogram{
+		'A': 0,
+		'C': 0,
+		'G': 0,
+		'T': 0,
+	}
+}
+
+// DNA is a list of nucleotides.
+type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
@@ -14,6 +24,15 @@ type DNA
 // The receiver appears in its own argument list between the func keyword and the method name.
 // Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram
+	var h Histogram = newHistogram()
+
+	for _, nucleotide := range d {
+		if _, isValid := h[nucleotide]; !isValid {
+			return newHistogram(), errors.New("invalid DNA")
+		}
+
+		h[nucleotide]++
+	}
+
 	return h, nil
 }
