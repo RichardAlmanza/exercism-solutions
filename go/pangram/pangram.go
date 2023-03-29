ba6377394913e2476be8db498710cf2137e182c5
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-29 01:12:07 -0500

    go/pangram solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,39 @@
 package pangram
 
+import "strings"
+
+type alphabet map[rune]uint8
+
+const englishAlphabet string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func NewAlphabet(sAlphabet string) alphabet {
+	var english alphabet = make(alphabet, len(sAlphabet))
+
+	for _, runeKey := range sAlphabet {
+		english[runeKey] = 0
+	}
+
+	return english
+}
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	var counter alphabet = NewAlphabet(englishAlphabet)
+
+	input = strings.ToUpper(input)
+
+	for _, runeKey := range input {
+		_, exists :=  counter[runeKey]
+
+		if exists {
+			counter[runeKey]++
+		}
+	}
+
+	for _, value := range counter {
+		if value == 0 {
+			return false
+		}
+	}
+
+	return true
 }
