a6ae3451f7e837ab9f79313857ff4267babe95d9
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-28 19:20:58 -0500

    go/strain solution

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -5,17 +5,73 @@ type Lists [][]int
 type Strings []string
 
 func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+	var kept Ints
+
+	if i == nil {
+		return nil
+	}
+
+	kept = make(Ints, 0, len(i))
+
+	for _, value := range i {
+		if filter(value) {
+			kept = append(kept, value)
+		}
+	}
+
+	return kept
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+	var discarded Ints
+
+	if i == nil {
+		return nil
+	}
+
+	discarded = make(Ints, 0, len(i))
+
+	for _, value := range i {
+		if !filter(value) {
+			discarded = append(discarded, value)
+		}
+	}
+
+	return discarded
 }
 
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+	var kept Lists
+
+	if l == nil {
+		return nil
+	}
+
+	kept = make(Lists, 0, len(l))
+
+	for _, values := range l {
+		if filter(values) {
+			kept = append(kept, values)
+		}
+	}
+
+	return kept
 }
 
 func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+	var kept Strings
+
+	if s == nil {
+		return nil
+	}
+
+	kept = make(Strings, 0, len(s))
+
+	for _, value := range s {
+		if filter(value) {
+			kept = append(kept, value)
+		}
+	}
+
+	return kept
 }
