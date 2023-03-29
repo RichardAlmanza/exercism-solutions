51bd80d9ef1b5d5d41eab2e1a896c4cb33bcc014
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-29 02:49:12 -0500

    go/etl solution

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,15 @@
 package etl
 
+import "strings"
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	var out map[string]int = make(map[string]int, 26)
+
+	for score, letters := range in {
+		for _, letter := range letters {
+			out[strings.ToLower(letter)] = score
+		}
+	}
+
+	return out
 }
