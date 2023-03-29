a2dcfb832a98be7e2b2089074b1958520434741d
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-29 02:33:13 -0500

    go/darts solution

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,30 @@
 package darts
 
+import "math"
+
+func getScore(distanceFromCenter float64) int {
+	var score int
+
+	switch{
+	case distanceFromCenter <= 1.0:
+		score = 10
+	case distanceFromCenter <= 5.0:
+		score = 5
+	case distanceFromCenter <= 10.0:
+		score = 1
+	}
+
+	return score
+}
+
+func calculatesDistanceFromCenter(x, y float64) float64 {
+	return math.Pow(
+		math.Pow(x, 2) + math.Pow(y, 2),
+		0.5,
+	)
+}
+
+// Score returns the score of the dart landed at x and y coordinates
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	return getScore(calculatesDistanceFromCenter(x, y))
 }
