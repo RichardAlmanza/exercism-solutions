5fa54bb3e08cc554fa49cba5724eccd643d13c6a
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-29 04:31:31 -0500

    go/bob solution

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,60 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"regexp"
+	"strings"
+)
+
+const (
+	Asking uint8 = 1 << iota // 001
+	Yelling                  // 010
+	Silence                  // 100
+)
+
+var (
+	isAsking *regexp.Regexp = regexp.MustCompile(`\?$`)
+	hasWords *regexp.Regexp = regexp.MustCompile(`[A-Za-z]+`)
+)
+
+func getRemarkType(remark string) uint8 {
+	var remarkType uint8
+	remark = strings.TrimSpace(remark)
+
+	if remark == "" {
+		remarkType = remarkType | Silence
+	}
+
+	if isAsking.MatchString(remark) {
+		remarkType = remarkType | Asking
+	}
+
+	if hasWords.MatchString(remark) && remark == strings.ToUpper(remark) {
+		remarkType = remarkType | Yelling
+	}
+
+	return remarkType
+}
+
+func reply(remarkType uint8) string {
+	var response string
+
+	switch remarkType {
+	case Silence:
+		response = "Fine. Be that way!"
+	case Yelling:
+		response = "Whoa, chill out!"
+	case Asking:
+		response = "Sure."
+	case Yelling | Asking:
+		response = "Calm down, I know what I'm doing!"
+	default:
+		response = "Whatever."
+	}
+
+	return response
+}
+
+// Hey returns the bob's reply for remark
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	return reply(getRemarkType(remark))
 }
