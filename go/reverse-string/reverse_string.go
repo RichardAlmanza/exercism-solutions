d88585e6f64fd3a7bd04e761e1eb2eec86d2c0a1
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-29 02:06:34 -0500

    go/reverse-string solution

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,5 +1,17 @@
 package reverse
 
+import (
+	"strings"
+)
+
+// Reverse return the reversed input
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	var temp []rune = []rune(input)
+	var reversedInput strings.Builder
+
+	for index := len(temp) - 1; index >= 0; index-- {
+		reversedInput.WriteRune(temp[index])
+	}
+
+	return reversedInput.String()
 }
