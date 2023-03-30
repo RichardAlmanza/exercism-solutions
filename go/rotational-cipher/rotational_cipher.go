9c9fe221b81ab359426acc509087b2c43fa662f0
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-30 18:30:21 -0500

    go/rotational-cipher solution

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,5 +1,33 @@
 package rotationalcipher
 
+import "strings"
+
+func rotate(character rune, shiftKey int) rune {
+	var base rune
+
+	switch {
+	case 'A' <= character && character <= 'Z':
+		base = 'A'
+	case 'a' <= character && character <= 'z':
+		base = 'a'
+	default:
+		return character
+	}
+
+	character -= base
+	character += rune(shiftKey)
+	character %= 26
+	character += base
+
+	return character
+}
+
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	var ciphered strings.Builder
+
+	for _, character := range plain {
+		ciphered.WriteRune(rotate(character, shiftKey))
+	}
+
+	return ciphered.String()
 }
