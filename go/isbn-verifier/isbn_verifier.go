7453185260286a90a091893d19f7b9aa345329c2
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-30 16:55:30 -0500

    go/isbn-verifier solution

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,35 +1,33 @@
 package isbn
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+// using regex, it is just a little less efficient but more readable than my previous solution
+var isbnStructure *regexp.Regexp = regexp.MustCompile(`^\d{9}[xX\d]$`)
 
 func IsValidISBN(isbn string) bool {
 	var cleanedISBN []rune
 	var sum int
 	var value int
 
-	isbn = strings.ToUpper(isbn)
-	cleanedISBN = []rune(strings.ReplaceAll(isbn, "-", ""))
+	isbn = strings.ReplaceAll(isbn, "-", "")
 
-	if len(cleanedISBN) != 10 {
+	if !isbnStructure.MatchString(isbn) {
 		return false
 	}
 
-	for index, r := range cleanedISBN {
-		runeIsX := r == 'X'
+	cleanedISBN = []rune(isbn)
 
-		if r < '0' && '9' > r && !runeIsX{
-			return false
-		}
+	if !unicode.IsDigit(cleanedISBN[9]) {
+		cleanedISBN[9] = '9' + 1
+	}
 
-		if runeIsX && index < 9 {
-			return false
-		}
-
-		if runeIsX {
-			r = '9' + 1
-		}
-
-		value = int(r - '0')
+	for index, character := range cleanedISBN {
+		value = int(character - '0')
 
 		sum += value * (10 - index)
 	}
