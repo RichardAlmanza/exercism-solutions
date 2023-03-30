ee2e08c75b15a3b99d449d1b0ed314a4561d244f
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-30 16:04:08 -0500

    go/isbn-verifier solution

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,38 @@
 package isbn
 
+import "strings"
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	var cleanedISBN []rune
+	var sum int
+	var value int
+
+	isbn = strings.ToUpper(isbn)
+	cleanedISBN = []rune(strings.ReplaceAll(isbn, "-", ""))
+
+	if len(cleanedISBN) != 10 {
+		return false
+	}
+
+	for index, r := range cleanedISBN {
+		runeIsX := r == 'X'
+
+		if r < '0' && '9' > r && !runeIsX{
+			return false
+		}
+
+		if runeIsX && index < 9 {
+			return false
+		}
+
+		if runeIsX {
+			r = '9' + 1
+		}
+
+		value = int(r - '0')
+
+		sum += value * (10 - index)
+	}
+
+	return sum % 11 == 0
 }
