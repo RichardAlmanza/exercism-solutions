76b9b623459dea03eaf1cc251e877d36bd98ea95
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-29 22:01:16 -0500

    go/grains solution

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,23 @@
 package grains
 
+import (
+	"errors"
+	"math"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number < 1 {
+		return 0, errors.New("why would you ask for 0 squares or less")
+	}
+
+	if number > 64 {
+		return 0, errors.New("there are no more than 64 squares")
+	}
+
+	return uint64(math.Pow(2, float64(number - 1))), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	totalGrains, _ := Square(64)
+	return totalGrains * 2 - 1
 }
