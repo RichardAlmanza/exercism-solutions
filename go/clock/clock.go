d5a4a933224546c3d741d32c7bb16e5be6615888
Richard Almanza <richardalmanza.dev@gmail.com>
2023-03-31 23:54:30 -0500

    go/clock solution

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,19 +1,35 @@
 package clock
 
+import "fmt"
+
 // Define the Clock type here.
+type Clock int
+
+const maxMinutes int = 1440
+const clockFormat string = "%.2d:%.2d"
+
+func (c Clock) mod() Clock {
+	c %= Clock(maxMinutes)
+
+	if c < 0 {
+		c += Clock(maxMinutes)
+	}
+
+	return c
+}
 
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	return Clock(m + h * 60).mod()
 }
 
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	return (c + Clock(m)).mod()
 }
 
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	return (c - Clock(m)).mod()
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	return fmt.Sprintf(clockFormat, c / 60, c % 60)
 }
