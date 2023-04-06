2e43f98bc43b4d9949a5505686848600b75476bd
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-06 18:16:59 -0500

    go/largest-series-product solution

--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,5 +1,63 @@
 package lsproduct
 
+import "errors"
+
+var (
+	ErrSmallSpan error = errors.New("span must be smaller than string length")
+	ErrNegativeSpan error = errors.New("span must not be negative")
+	ErrNotDigit error = errors.New("digits input must only contain digits")
+)
+
+func isDigit(digit rune) bool {
+	return '0' <= digit && digit <= '9'
+}
+
+func rDigitToInt64 (digit rune) int64 {
+	return int64(digit - '0')
+}
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	var factorCounter int
+	var maxProduct int64
+	var product int64 = 1
+	var digitChars []rune = []rune(digits)
+
+	if span < 0 {
+		return 0, ErrNegativeSpan
+	}
+
+	if len(digitChars) < span {
+		return 0, ErrSmallSpan
+	}
+
+	if span == 0 {
+		return 1, nil
+	}
+
+	for i := 0; i < len(digitChars); i++ {
+		digitChar := digitChars[i]
+
+		if !isDigit(digitChar) {
+			return 0, ErrNotDigit
+		}
+
+		if digitChar == '0' {
+			product = 1
+			factorCounter = 0
+			continue
+		}
+
+		product *= rDigitToInt64(digitChar)
+		factorCounter++
+
+		if factorCounter > span {
+			product /= rDigitToInt64(digitChars[i-span])
+		}
+
+		if product > maxProduct && factorCounter >= span {
+			maxProduct = product
+		}
+	}
+
+	return maxProduct, nil
 }
