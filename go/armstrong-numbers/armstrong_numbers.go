2f29bb4593c63089145d0df8e9458e3c65b7b26c
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-07 02:12:04 -0500

    go/armstrong-numbers solution

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,35 @@
 package armstrong
 
+import "math"
+
+func countDigits(n int) int {
+	var counter int = 1
+
+	for n > 9 {
+		n /= 10
+		counter++
+	}
+
+	return counter
+}
+
+func powInt(base, exponent int) int {
+	return int(
+		math.Pow(
+			float64(base), float64(exponent),
+		),
+	)
+}
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	var checkSum int
+	var tempN int = n
+	var nDigits int = countDigits(n)
+
+	for i := 0; i < nDigits; i++ {
+		checkSum += powInt(tempN % 10, nDigits)
+		tempN /= 10
+	}
+
+	return checkSum == n
 }
