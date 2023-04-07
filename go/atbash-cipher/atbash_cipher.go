fb5a0678c8ae9c938874cfaf33e6123b14b40009
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-06 22:32:27 -0500

    go/atbash-cipher solution

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,38 @@
 package atbash
 
+import (
+	"strings"
+	"unicode"
+)
+
+var groupSize int = 5
+var alphabet []rune = []rune("abcdefghijklmnopqrstuvwxyz")
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	var ciphered strings.Builder
+	var counter int
+
+	for _, char := range s {
+		if !unicode.IsLetter(char) && !unicode.IsDigit(char) {
+			continue
+		}
+
+		if unicode.IsLetter(char) {
+			char = unicode.ToLower(char)
+			
+			index := int(char - 'a' + 1)
+			index = len(alphabet) - index
+
+			char = alphabet[index]
+		}
+
+		if counter % groupSize == 0 && counter > 0 {
+			ciphered.WriteRune(' ')
+		}
+
+		ciphered.WriteRune(char)
+		counter++
+	}
+
+	return ciphered.String()
 }
