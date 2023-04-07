0f1098e3c795a5724239f7e9764c739b37bd1974
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-07 01:43:46 -0500

    go/sieve solution

--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,5 +1,25 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	var numbers []bool
+	var primes []int
+
+	limit++
+
+	numbers = make([]bool, limit)
+	primes = make([]int, 0, limit)
+
+	for i := 2; i < limit; i++ {
+		if numbers[i] { // is Composite?
+			continue
+		}
+
+		for ii := 2; ii * i < limit; ii++ {
+			numbers[i * ii] = true // all theses numbers are composite
+		}
+
+		primes = append(primes, i)
+	}
+
+	return primes
 }
