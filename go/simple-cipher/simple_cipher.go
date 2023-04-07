6d5cbc34fb03d95c5f3fe549c54582b0abcb3bb3
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-07 00:46:58 -0500

    go/simple-cipher solution

--- go/simple-cipher/simple_cipher.go
+++ go/simple-cipher/simple_cipher.go
@@ -1,32 +1,110 @@
 package cipher
 
-// Define the shift and vigenere types here.
-// Both types should satisfy the Cipher interface.
+import (
+	"strings"
+	"unicode"
+)
+
+type shift int8
+type vigenere []rune
+
+
+func encodeLetter(letter, key rune) rune {
+	letter = unicode.ToLower(letter)
+	letter = (key + letter - 'a') % 26 + 'a'
+
+	return letter
+}
 
 func NewCaesar() Cipher {
-	panic("Please implement the NewCaesar function")
+	return NewShift(3)
 }
 
 func NewShift(distance int) Cipher {
-	panic("Please implement the NewShift function")
+	if distance == 0 {
+		return nil
+	}
+
+	if distance < -25 || 25 < distance  {
+		return nil
+	}
+
+	if distance < 0 {
+		distance += 26
+	}
+
+	return shift(distance)
 }
 
 func (c shift) Encode(input string) string {
-	panic("Please implement the Encode function")
+	var encoded strings.Builder
+
+	for _, char := range input {
+		if !unicode.IsLetter(char) {
+			continue
+		}
+
+		char = encodeLetter(char, rune(c))
+
+		encoded.WriteRune(char)
+	}
+
+	return encoded.String()
 }
 
 func (c shift) Decode(input string) string {
-	panic("Please implement the Decode function")
+	return shift(26 - int(c)).Encode(input)
 }
 
 func NewVigenere(key string) Cipher {
-	panic("Please implement the NewVigenere function")
+	var compiled strings.Builder
+	var checkSum rune
+
+	for _, char := range key {
+		if char < 'a' || 'z' < char {
+			return nil
+		}
+
+		char -= 'a'
+		checkSum += char
+
+		compiled.WriteRune(char)
+	}
+
+	if checkSum == '\x00' {
+		return nil
+	}
+
+	return vigenere(compiled.String())
 }
 
 func (v vigenere) Encode(input string) string {
-	panic("Please implement the Encode function")
+	var encoded strings.Builder
+	var index int
+
+	for _, char := range input {
+		if !unicode.IsLetter(char) {
+			continue
+		}
+
+		index %= len(v)
+
+		char = encodeLetter(char, v[index])
+
+		encoded.WriteRune(char)
+
+		index++
+	}
+
+	return encoded.String()
 }
 
 func (v vigenere) Decode(input string) string {
-	panic("Please implement the Decode function")
+	var decoderKey vigenere = make(vigenere, len(v))
+
+	for i, key := range v {
+		decoderKey[i] = 26 - key
+	}
+
+	return decoderKey.Encode(input)
 }
