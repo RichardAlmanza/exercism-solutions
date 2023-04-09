71f89f16dae87e34fc7d63691251c0cf5ae98f8f
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-09 18:45:25 -0500

    go/sublist solution

--- go/sublist/sublist.go
+++ go/sublist/sublist.go
@@ -2,6 +2,60 @@ package sublist
 
 // Relation type is defined in relations.go file.
 
+func areEqual(l1, l2 []int) Relation {
+	for i, v1 := range l1 {
+		v2 := l2[i]
+
+		if v1 != v2 {
+			return RelationUnequal
+		}
+	}
+
+	return RelationEqual
+}
+
+func isSubList(l1, l2 []int) bool {
+	// l2 in l1
+	if len(l2) == 0 {
+		return true
+	}
+
+loopL1:
+	for i := 0; i < len(l1); i++ {
+		if l1[i] == l2[0] {
+			for j := 0; j < len(l2); j++ {
+				if l1[i + j] != l2[j] {
+					continue loopL1
+				}
+			}
+
+			return true
+		}
+	}
+
+	return false
+}
+
+func aRelationB(l1, l2 []int, relationAB Relation) Relation {
+	if isSubList(l1, l2) {
+		return relationAB
+	}
+
+	return RelationUnequal
+}
+
 func Sublist(l1, l2 []int) Relation {
-	panic("Please implement the Sublist function")
+	var result Relation
+	var lenDifference int = len(l1) - len(l2)
+
+	switch {
+	case lenDifference == 0:
+		result = areEqual(l1, l2)
+	case lenDifference < 0:
+		result = aRelationB(l2, l1, RelationSublist)
+	default:
+		result = aRelationB(l1, l2, RelationSuperlist)
+	}
+
+	return result
 }
