b55173cd10113d6110ff6d8dd565f88931984e19
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-09 16:56:08 -0500

    go/series solution

--- go/series/series.go
+++ go/series/series.go
@@ -1,9 +1,39 @@
 package series
 
+import "strings"
+
 func All(n int, s string) []string {
-	panic("Please implement the All function")
+	var results []string
+	var digits []rune = []rune(s)
+
+	if n > len(digits) {
+		return nil
+	}
+
+	for i := n; i <= len(digits); i++ {
+		results = append(results, string(digits[i - n: i]))
+	}
+
+	return results
+}
+
+func First(n int, s string) (string, bool) {
+	var result strings.Builder
+
+	for _, digit := range s {
+		if n == 0 {
+			break
+		}
+
+		result.WriteRune(digit)
+		n--
+	}
+
+	return result.String(), n == 0
 }
 
 func UnsafeFirst(n int, s string) string {
-	panic("Please implement the UnsafeFirst function")
+	result, _ := First(n, s)
+
+	return result
 }
