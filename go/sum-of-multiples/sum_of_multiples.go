4a39ded110b30674aa77f377884d7ebfeae7bc35
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-11 19:54:33 -0500

    go/sum-of-multiples solution

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,28 @@
 package summultiples
 
+type multiples map[int]bool
+
+func (m multiples) add(divisor, limit int) {
+	if divisor == 0 {
+		return
+	}
+
+	for factor := divisor; factor < limit; factor += divisor {
+		m[factor] = true
+	}
+}
+
 func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+	var sum int
+	var unique multiples = make(multiples)
+
+	for _, divisor := range divisors {
+		unique.add(divisor, limit)
+	}
+
+	for key := range unique {
+		sum += key
+	}
+
+	return sum
 }
