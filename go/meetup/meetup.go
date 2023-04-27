1a8caf2ea29be05d6f589cacbd639f46fc3a8cc0
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-26 23:30:18 -0500

    go/meetup solution

--- go/meetup/meetup.go
+++ go/meetup/meetup.go
@@ -2,8 +2,75 @@ package meetup
 
 import "time"
 
-// Define the WeekSchedule type here.
+type WeekSchedule int
+
+const (
+	First WeekSchedule = iota
+	Second
+	Third
+	Fourth
+	Teenth
+	Last
+)
+
+func firstDay(date time.Time) time.Time {
+	if date.Day() != 1 {
+		date = time.Date(
+			date.Year(), date.Month(), 1,
+			date.Hour(), date.Minute(), date.Second(), date.Nanosecond(),
+			date.Location(),
+		)
+	}
+
+	return date
+}
+
+func moveToFirstWDay(date time.Time, wDay time.Weekday) time.Time {
+	var diffDay time.Duration = time.Duration(wDay - date.Weekday())
+
+	if diffDay < 0 {
+		diffDay	+= 7
+	}
+
+	date = firstDay(date)
+	date = date.Add(24 * time.Hour * diffDay)
+
+	return date
+}
+
+func nextWeek(date time.Time) time.Time {
+	date = date.Add(24 * time.Hour * 7)
+
+	return date
+}
+
+func prevWeek(date time.Time) time.Time {
+	date = date.Add(-24 * time.Hour * 7)
+
+	return date
+}
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	var date time.Time = time.Date(year, month, 1, 12, 0, 0, 0, time.UTC)
+
+	date = moveToFirstWDay(date, wDay)
+
+	switch wSched {
+	case Second, Third, Fourth:
+		for i := 0; i < int(wSched); i++ {
+			date = nextWeek(date)
+		}
+	case Teenth:
+		for date.Day() < 13 {
+			date = nextWeek(date)
+		}
+	case Last:
+		for date.Month() == month {
+			date = nextWeek(date)
+		}
+
+		date = prevWeek(date)
+	}
+
+	return date.Day()
 }
