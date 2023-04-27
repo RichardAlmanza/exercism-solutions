56c1056d8b95f272d1470e3537c68e0558379581
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-26 23:57:45 -0500

    go/meetup solution

--- go/meetup/meetup.go
+++ go/meetup/meetup.go
@@ -13,63 +13,43 @@ const (
 	Last
 )
 
-func firstDay(date time.Time) time.Time {
-	if date.Day() != 1 {
-		date = time.Date(
-			date.Year(), date.Month(), 1,
-			date.Hour(), date.Minute(), date.Second(), date.Nanosecond(),
-			date.Location(),
-		)
-	}
-
-	return date
-}
-
-func moveToFirstWDay(date time.Time, wDay time.Weekday) time.Time {
+func moveToWDay(date time.Time, wDay time.Weekday) time.Time {
 	var diffDay time.Duration = time.Duration(wDay - date.Weekday())
 
 	if diffDay < 0 {
 		diffDay	+= 7
 	}
 
-	date = firstDay(date)
 	date = date.Add(24 * time.Hour * diffDay)
 
 	return date
 }
 
-func nextWeek(date time.Time) time.Time {
-	date = date.Add(24 * time.Hour * 7)
-
-	return date
-}
-
-func prevWeek(date time.Time) time.Time {
-	date = date.Add(-24 * time.Hour * 7)
+func moveWeek(date time.Time, nWeeks time.Duration) time.Time {
+	date = date.Add(24 * time.Hour * 7 * nWeeks)
 
 	return date
 }
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	var date time.Time = time.Date(year, month, 1, 12, 0, 0, 0, time.UTC)
+	var date time.Time
+	var day int = 1
 
-	date = moveToFirstWDay(date, wDay)
+	switch wSched {
+	case Last:
+		month++
+	case Teenth:
+		day = 13
+	}
+
+	date = time.Date(year, month, day, 12, 0, 0, 0, time.UTC)
+	date = moveToWDay(date, wDay)
 
 	switch wSched {
 	case Second, Third, Fourth:
-		for i := 0; i < int(wSched); i++ {
-			date = nextWeek(date)
-		}
-	case Teenth:
-		for date.Day() < 13 {
-			date = nextWeek(date)
-		}
+		date = moveWeek(date, time.Duration(wSched))
 	case Last:
-		for date.Month() == month {
-			date = nextWeek(date)
-		}
-
-		date = prevWeek(date)
+		date = moveWeek(date, -1)
 	}
 
 	return date.Day()
