89b9dd3a91c86bca52a4f46adb8d07f2f11aa864
Richard Almanza <richardalmanza.dev@gmail.com>
2023-04-29 16:33:43 -0500

    go/flatten-array solution

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,5 +1,27 @@
 package flatten
 
+func flat(flatted []interface{}, nested interface{}) []interface{} {
+	if nested == nil {
+		return flatted
+	}
+
+	subNested, isSlice := nested.([]interface{});
+
+	if isSlice {
+		for _, value := range subNested {
+			flatted = flat(flatted, value)
+		}
+	} else {
+		flatted = append(flatted, nested)
+	}
+
+	return flatted
+}
+
 func Flatten(nested interface{}) []interface{} {
-	panic("Please implement the Flatten function")
+	var flatted = []interface{}{}
+
+	flatted = flat(flatted, nested)
+
+	return flatted
 }
